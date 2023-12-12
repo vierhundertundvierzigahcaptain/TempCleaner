cf8c0960c6979a5123ed6481c5ccac8bf4c43f16
vierhundertundvierzigahcaptain <exhed404@gmail.com>
2023-12-12 20:11:14 +0400

    Added more pathes, cleaning principle changed

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,39 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let path = String::from(format!("C:/Users/{}/AppData/Local/Temp", whoami::username())); // user temp
-    clean(path);
-    let path = String::from("C:/Windows/Temp"); // windows temp
-    clean(path);
-    let path = String::from("C:/Windows/LiveKernelReports"); // kernel reports
-    clean(path);
-    let path = String::from("C:/Windows/Downloaded Program Files"); // trash for internet explorer, may also contain viruses
-    clean(path);
-    let path = String::from("C:/Windows/SoftwareDistribution"); // old windows updates
-    clean(path);
-    let path = String::from("C:/Windows/Minidump"); // dumps
-    clean(path);
-    let path = String::from("C:/Windows/Prefetch"); // only trash
-    clean(path);
-    let path = String::from("C:/Windows/Logs"); // logs
-    clean(path);
+    let pathes = vec![
+        String::from(format!("C:/Users/{}/AppData/Local/Temp", whoami::username())), // user temp
+        String::from("C:/Windows/Temp"), // windows temp
+        String::from("C:/Windows/LiveKernelReports"), // kernel reports
+        String::from("C:/Windows/Downloaded Program Files"), // trash for internet explorer, may also contain viruses
+        String::from("C:/Windows/SoftwareDistribution"), // old windows updates
+        String::from("C:/Windows/Prefetch"), // only trash
+        String::from("C:/Windows/Logs"), // logs
+        String::from(format!("C:/Users/{}/AppData/Local/DBG", whoami::username())), // PDB cache
+        String::from(format!("C:/Users/{}/AppData/Local/Microsoft/Windows/Explorer", whoami::username())), // thumbnail cache
+        String::from("C:/ProgramData/Microsoft/Windows/WER"), // Windows reports
+        // dumps
+        String::from("C:/Windows/Minidump"),
+        String::from(format!("C:/users/{}/appdata/local/CrashDumps", whoami::username())),
+        // temporary driver unpacking folders
+        String::from("C:/AMD"),
+        String::from("C:/Intel"),
+    ];
+
+    for path in pathes.iter() {
+        clean(path);
+    }
 }
 
-fn clean(path: String) {
+fn clean(path: &String) {
     let dir = Path::new(&path);
 
-    let files = dir.read_dir().unwrap();
-
-    for file in files {
-        let file_path = file.unwrap().path();
-        match fs::remove_file(file_path.clone()) {
-            Ok(_) => (),
-            Err(_) => (),
-        }
-        match fs::remove_dir_all(file_path) {
-            Ok(_) => (),
-            Err(_) => (),
+    if dir.exists() {
+        let files = dir.read_dir().unwrap();
+        for file in files {
+            let file_path = file.unwrap().path();
+            if let Err(_) = fs::remove_file(file_path.clone()) {}
+            if let Err(_) = fs::remove_dir_all(file_path) {}
         }
     }
 }
